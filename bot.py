1842af0de131b3399f3bb18b5ceec55b8691da3f
Arman <arman.aghababyan.1301@gmail.com>
2024-06-27 16:07:59 +0400

    ок

--- bot.py
+++ bot.py
@@ -1,7 +1,3 @@
-import cv2
-import numpy as np
-import os
-from math import sqrt, ceil, floor
 from telebot import TeleBot
 from confing import TOKEN
 
@@ -34,47 +30,6 @@ def callback_query(call):
 
     else:
         bot.send_message(user_id, "Сумма всех победителей достигла 3, вы не можете быть добавлены.")
-
-def get_winners_img(self, user_id):
-    conn = sqlite3.connect(self.database)
-    with conn:
-        cur = conn.cursor()
-        cur.execute(''' 
-SELECT image FROM winners 
-INNER JOIN prizes ON 
-winners.prize_id = prizes.prize_id
-WHERE user_id = ?''', (user_id, ))
-        return cur.fetchall()
-
-def create_collage(image_paths):
-    images = []
-    for path in image_paths:
-        image = cv2.imread(path)
-        images.append(image)
-
-    num_images = len(images)
-    num_cols = floor(sqrt(num_images)) # Поиск количество картинок по горизонтали
-    num_rows = ceil(num_images/num_cols)  # Поиск количество картинок по вертикали
-    # Создание пустого коллажа
-    collage = np.zeros((num_rows * images[0].shape[0], num_cols * images[0].shape[1], 3), dtype=np.uint8)
-    # Размещение изображений на коллаже
-    for i, image in enumerate(images):
-        row = i // num_cols
-        col = i % num_cols
-        collage[row*image.shape[0]:(row+1)*image.shape[0], col*image.shape[1]:(col+1)*image.shape[1], :] = image
-    return collage
-
-
-m = DatabaseManager(DATABASE)
-info = m.get_winners_img("user_id")
-prizes = [x[0] for x in info]
-image_paths = os.listdir('img')
-image_paths = [f'img/{x}' if x in prizes else f'hidden_img/{x}' for x in image_paths]
-collage = create_collage(image_paths)
-
-cv2.imshow('Collage', collage)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
 
 if __name__ == "__main__":
     bot.polling(none_stop=True)
